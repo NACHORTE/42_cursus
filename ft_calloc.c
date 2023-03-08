7ca63ff09d5bbd5b1e34c0ce5352c3c08aeeef45
NACHORTE <ignaciortegape@gmail.com>
2023-03-08 16:40:28 +0100

    makefile

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iortega- <iortega-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 16:28:20 by iortega-          #+#    #+#             */
+/*   Updated: 2023/03/08 16:28:20 by iortega-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*str;
+
+	if (nmemb <= 0 || size <= 0)
+		str = (char *)malloc(1);
+	else
+		str = (char *)malloc(nmemb * size);
+	if (!str)
+		return (NULL);
+	return (ft_memset(str, 0, nmemb * size));
+}
