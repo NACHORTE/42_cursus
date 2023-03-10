ca32c6e9b42ab80eccba26d26430630d84c41299
NACHORTE <ignaciortegape@gmail.com>
2023-03-10 17:10:15 +0100

    added bonus

---
+++ ft_lstmap.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iortega- <iortega-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 16:02:54 by iortega-          #+#    #+#             */
+/*   Updated: 2023/03/10 16:02:54 by iortega-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*start;
+
+	new = 0;
+	start = ft_lstnew(f(lst->content));
+	if (!start)
+		return (NULL);
+	new = start->next;
+	lst = lst->next;
+	while (lst)
+	{
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&start, del);
+			return (NULL);
+		}
+		new = new->next;
+		lst = lst->next;
+	}
+	return (start);
+}
--- libft.h
+++ libft.h
@@ -17,6 +17,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 int				ft_atoi(const char *str);
 void			*ft_bzero(void *s, size_t n);
 void			*ft_calloc(size_t nmemb, size_t size);
@@ -53,5 +59,16 @@ char			*ft_strupcase(char *str);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
+
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+void			ft_lstclear(t_list **lst, void (*del)(void*));
+void			ft_lstdelone(t_list *lst, void (*del)(void*));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list			*ft_lstlast(t_list *lst);
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), \
+					void (*del)(void *));
+t_list			*ft_lstnew(void *content);
+int				ft_lstsize(t_list *lst);
 
 #endif
